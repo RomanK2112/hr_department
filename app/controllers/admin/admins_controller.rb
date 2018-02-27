b520cde504eebbb2f152647d8104849f0382a899
serjpetrenko <b.g.lupus@gmail.com>
2018-02-27 10:25:48 +0200

    Add admin pages and navbar with link to admin panel

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -2,6 +2,11 @@ class Admin::AdminsController < ActionController::Base
   before_action :authenticate_user!
   layout 'admin'
 
+  def index
+  end
+
+  private
+
   def authenticate_user!
     redirect_to root_path, alert: 'Not authorized.' if !signed_in?
   end
