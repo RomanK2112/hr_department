2e3d6962c49158148634c8017e064e6ef6f18c1f
serjpetrenko <b.g.lupus@gmail.com>
2018-02-28 11:22:21 +0200

    Add seeds, remove useless static controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+admin = User.create_with(
+  first_name: 'John',
+  last_name: 'Doe',
+  password: 'Test1234!',
+  is_admin: true
+).find_or_create_by(email: 'john@gmail.com')
+
+employee1 = User.create(
+  first_name: 'Bob',
+  last_name: 'Worker',
+  password: 'New1234!'
+).find_or_create_by(email: 'bob@gmail.com')
+
+employee2 = User.create(
+  first_name: 'Jack',
+  last_name: 'Programmer',
+  password: 'New1234!'
+).find_or_create_by(email: 'jack@gmail.com')
+
+post1 = admin.posts.create(
+  title: 'New post for rubyists',
+  body: 'Here some useful links to articles'
+)
+
+post2 = admin.posts.create(
+  title: 'Best practices for building API',
+  body: "You'll find usefull info on these links"
+)
+
+group1 = Group.create(name: 'Ruby team', description: 'Rails developers')
+group2 = Group.create(name: 'Golang team', description: 'Golang developers')
+
+group1.posts << post1
+group2.posts << post1
+
+group1.posts << post2
+group2.posts << post2
