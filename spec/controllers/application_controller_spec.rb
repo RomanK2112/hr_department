8a17a92c6666e6000e3ef44344c76b301d8080d5
serjpetrenko <b.g.lupus@gmail.com>
2018-06-11 00:09:54 +0300

    Add spec for application controller

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationController, type: :controller do
+  controller do
+    def after_sign_in_path_for(resource)
+      super resource
+    end
+  end
+
+  let(:admin) { FactoryBot.create(:admin) }
+  let(:employee) { FactoryBot.create(:user) }
+
+  describe '#after_sign_in_path_for' do
+    context 'when admin' do
+      it 'redirects to admin path' do
+        sign_in admin
+        expect(controller.after_sign_in_path_for(admin)).to match '/admin/admins'
+      end
+    end
+
+    context 'when employee' do
+      it 'redirects to employee path' do
+        sign_in employee
+        expect(controller.after_sign_in_path_for(employee)).to match employees_path(employee.id)
+      end
+    end
+  end  
+end
